761512024abf4b31ca335ca044b336880f8034fc
llliiivvveee <382706992@qq.com>
2018-08-08 16:13:33 +0800

    修改数据库

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -10,19 +10,29 @@ See the License for the specific language governing permissions and limitations 
 
 用于正式环境的全局配置
 """
-from settings import APP_ID
+# from settings import APP_ID
+#
+#
+# # ===============================================================================
+# # 数据库设置, 正式环境数据库设置
+# # ===============================================================================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+#         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+#         'USER': 'bk',                            # 你的数据库user
+#         'PASSWORD': 'Aa123456',                        # 你的数据库password
+#         'HOST': '10.17.128.162',                   		   # 数据库HOST
+#         'PORT': '3306',                        # 默认3306
+#     },
+# }
 
+import os
+from settings import BASE_DIR
 
-# ===============================================================================
-# 数据库设置, 正式环境数据库设置
-# ===============================================================================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'bk',                            # 你的数据库user
-        'PASSWORD': 'Aa123456',                        # 你的数据库password
-        'HOST': '10.17.128.162',                   		   # 数据库HOST
-        'PORT': '3306',                        # 默认3306
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
 }
